bce645f674303f49cd26a90640b3fb8020fc5a8d
andrewauclair <mightymalakai33@gmail.com>
2019-06-16 14:42:48 -0400

    Tasks will now be persisted to a tasks.txt file. Makes it usable between runs. All this code is temporary and will be replaced with a much better system in the future.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,53 @@
 // Copyright (C) 2019 Andrew Auclair - All Rights Reserved
 
+import java.io.*;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Tasks tasks = new Tasks();
+		
+		// reload previous tasks from a file
+		File file = new File("tasks.txt");
+		if (file.exists()) {
+			BufferedReader reader = new BufferedReader(new FileReader(new File("tasks.txt")));
+			
+			String line;
+			do {
+				line = reader.readLine();
+				if (line != null && !line.isEmpty()) {
+					tasks.addTask(line);
+				}
+			}
+			while (line != null);
+		}
 		
 		String command;
 		Scanner scanner = new Scanner(System.in);
 		do {
 			command = scanner.nextLine();
 			
-			try {
-				parseCommand(tasks, command);
-			}
-			catch (RuntimeException e) {
-				System.out.println(e.getMessage());
+			if (!command.equals("exit")) {
+				try {
+					parseCommand(tasks, command);
+				}
+				catch (RuntimeException e) {
+					System.out.println(e.getMessage());
+				}
 			}
 		}
 		while (!command.equals("exit"));
+		
+		// save to a file
+		// TODO we'll do this better in the future, right now this will lose all the current id's, you can't persist the active task with this
+		FileOutputStream output = new FileOutputStream(new File("tasks.txt"));
+		BufferedOutputStream outputStream = new BufferedOutputStream(output);
+		
+		for (Tasks.Task task : tasks.getTasks()) {
+			output.write(task.task.getBytes());
+			output.write("\n".getBytes());
+		}
 	}
 	
 	private static void parseCommand(Tasks tasks, String command) {
