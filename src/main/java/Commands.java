9ee8db671b135e4947412ef55f72e2e4035cd3c9
andrewauclair <mightymalakai33@gmail.com>
2019-06-17 21:22:03 -0400

    More cleanup. Commands are now functions that are added to a map. This should be more scalable.

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,69 +1,93 @@
 // Copyright (C) 2019 Andrew Auclair - All Rights Reserved
 
 import java.io.PrintStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 class Commands {
 	private final Tasks tasks;
 	private final PrintStream output;
-
+	
+	private interface Command {
+		void execute(String command);
+	}
+	
+	private final Map<String, Command> commands = new HashMap<>();
+	
 	Commands(Tasks tasks, PrintStream output) {
 		this.tasks = tasks;
 		this.output = output;
+		
+		commands.put("finish", command -> finishCommand());
+		commands.put("start", this::startCommand);
+		commands.put("stop", command -> stopCommand());
+		commands.put("add", this::addCommand);
+		commands.put("active", command -> activeCommand());
+		commands.put("list", command -> listCommand());
 	}
-
+	
 	void execute(String command) {
-		if (command.equals("finish")) {
-			Task task = tasks.finishTask();
-
-			output.println("Finished task " + task);
+		commands.keySet().stream()
+				.filter(command::startsWith)
+				.findFirst()
+				.ifPresentOrElse(name -> commands.get(name).execute(command),
+						() -> output.println("Unknown command."));
+	}
+	
+	private void listCommand() {
+		List<Task> tasksList = tasks.getTasks();
+		
+		Task activeTask = new Task(-1, "");
+		try {
+			activeTask = tasks.getActiveTask();
 		}
-		else if (command.startsWith("start")) {
-			String[] s = command.split(" ");
-			int taskID = Integer.parseInt(s[1]);
-
-			Task task = tasks.startTask(taskID);
-
-			output.println("Started task " + task);
+		catch (RuntimeException ignored) {
 		}
-		else if (command.equals("stop")) {
-			Task task = tasks.stopTask();
-			
-			output.println("Stopped task " + task);
+		
+		for (Task task : tasksList) {
+			output.print(task.id == activeTask.id ? "* " : "  ");
+			output.println(task);
 		}
-		else if (command.startsWith("add")) {
-			String task = command.substring(5, command.length() - 1);
-
-			int newTaskID = tasks.addTask(task);
-
-			output.println("Added task " + newTaskID + " \"" + task + "\"");
+		
+		if (tasksList.size() == 0) {
+			output.println("No tasks.");
 		}
-		else if (command.startsWith("active")) {
-			Task task = tasks.getActiveTask();
-			
-			output.println("Active task is " + task);
-		}
-		else if (command.startsWith("list")) {
-			List<Task> tasksList = tasks.getTasks();
-			
-			Task activeTask = new Task(-1, "");
-			try {
-				activeTask = tasks.getActiveTask();
-			}
-			catch (RuntimeException ignored) {
-			}
-			
-			for (Task task : tasksList) {
-				output.print(task.id == activeTask.id ? "* " : "  ");
-				output.println(task);
-			}
-			
-			if (tasksList.size() == 0) {
-				output.println("No tasks.");
-			}
-		}
-		else {
-			output.println("Unknown command.");
-		}
+	}
+	
+	private void activeCommand() {
+		Task task = tasks.getActiveTask();
+		
+		output.println("Active task is " + task);
+	}
+	
+	private void addCommand(String command) {
+		String task = command.substring(5, command.length() - 1);
+		
+		int newTaskID = tasks.addTask(task);
+		
+		output.println("Added task " + newTaskID + " \"" + task + "\"");
+	}
+	
+	private void stopCommand() {
+		Task task = tasks.stopTask();
+		
+		output.println("Stopped task " + task);
+	}
+	
+	private void startCommand(String command) {
+		String[] s = command.split(" ");
+		int taskID = Integer.parseInt(s[1]);
+		
+		Task task = tasks.startTask(taskID);
+		
+		output.println("Started task " + task);
+	}
+	
+	private void finishCommand() {
+		Task task = tasks.finishTask();
+		
+		output.println("Finished task " + task);
 	}
 }
