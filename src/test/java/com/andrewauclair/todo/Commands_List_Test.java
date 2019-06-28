8b7cfad159b0bab54cd241497116d293bb71fd80
Andrew Auclair <mightymalakai33@gmail.com>
2019-06-28 01:31:13 +0000

    Semi-temporary solution to being able to see all of the tasks in the list. Better commands to come later.

--- src/test/java/com/andrewauclair/todo/Commands_List_Test.java
+++ src/test/java/com/andrewauclair/todo/Commands_List_Test.java
@@ -52,7 +52,40 @@ class Commands_List_Test extends CommandsBaseTestCase {
 				"  20 - \"Test 20\"" + Utils.NL +
 				"(19 more tasks.)" + Utils.NL;
 
-		commands.execute("list");
+		commands.execute("list ignored");
+
+		assertEquals(expected, outputStream.toString());
+	}
+
+	@Test
+	void list_command_shows_all_tasks_when_all_parameter_is_passed() {
+		IntStream.range(1, 23)
+				.forEach(num -> tasks.addTask("Test " + num));
+
+		String expected = "  1 - \"Test 1\"" + Utils.NL +
+				"  2 - \"Test 2\"" + Utils.NL +
+				"  3 - \"Test 3\"" + Utils.NL +
+				"  4 - \"Test 4\"" + Utils.NL +
+				"  5 - \"Test 5\"" + Utils.NL +
+				"  6 - \"Test 6\"" + Utils.NL +
+				"  7 - \"Test 7\"" + Utils.NL +
+				"  8 - \"Test 8\"" + Utils.NL +
+				"  9 - \"Test 9\"" + Utils.NL +
+				"  10 - \"Test 10\"" + Utils.NL +
+				"  11 - \"Test 11\"" + Utils.NL +
+				"  12 - \"Test 12\"" + Utils.NL +
+				"  13 - \"Test 13\"" + Utils.NL +
+				"  14 - \"Test 14\"" + Utils.NL +
+				"  15 - \"Test 15\"" + Utils.NL +
+				"  16 - \"Test 16\"" + Utils.NL +
+				"  17 - \"Test 17\"" + Utils.NL +
+				"  18 - \"Test 18\"" + Utils.NL +
+				"  19 - \"Test 19\"" + Utils.NL +
+				"  20 - \"Test 20\"" + Utils.NL +
+				"  21 - \"Test 21\"" + Utils.NL +
+				"  22 - \"Test 22\"" + Utils.NL;
+
+		commands.execute("list --all");
 
 		assertEquals(expected, outputStream.toString());
 	}
