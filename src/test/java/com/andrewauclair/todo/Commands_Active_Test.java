e3a9fed900c7f3224009a40c2784f3206793a8d7
Andrew Auclair <mightymalakai33@gmail.com>
2019-06-27 20:34:54 -0400

    Active task now prints the time elapsed since it was last started.

--- src/test/java/com/andrewauclair/todo/Commands_Active_Test.java
+++ src/test/java/com/andrewauclair/todo/Commands_Active_Test.java
@@ -9,9 +9,12 @@ class Commands_Active_Test extends CommandsBaseTestCase {
 	@Test
 	void execute_active_command() {
 		tasks.addTask("Task 1");
+		setTime(1561078202);
 		tasks.startTask(1);
+		setTime(1561079202);
 		commands.execute("active");
 
-		assertEquals("Active task is 1 - \"Task 1\"" + System.lineSeparator(), outputStream.toString());
+		assertEquals("Active task is 1 - \"Task 1\"" + Utils.NL + Utils.NL +
+				"Current time elapsed: 00h 16m 40s" + Utils.NL, outputStream.toString());
 	}
 }
