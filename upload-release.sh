9156c55043d966b1edd06293bc59e21f2fbaa63d
Andrew Auclair <mightymalakai33@gmail.com>
2019-07-03 18:59:34 -0400

    Not sure what is wrong

--- upload-release.sh
+++ upload-release.sh
@@ -3,7 +3,7 @@
 echo $CI_COMMIT_TAG
 echo ""
 
-curl_upload_result=$(curl --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" --form "file=@target/todo-app-${CI_COMMIT_TAG}.one-jar.jar" https://gitlab.com/api/v4/projects/12882469/uploads)
+curl_upload_result=$(curl -v --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" --form "file=@target/todo-app-${CI_COMMIT_TAG}.one-jar.jar" https://gitlab.com/api/v4/projects/12882469/uploads)
 
 echo $curl_upload_result
 echo ""
@@ -16,7 +16,7 @@ echo ""
 echo $markdown
 echo ""
 
-curl_release_result=$(curl --request POST --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" --header "Content-Type: application/json" --data '{"description": "'$markdown'"}' https://gitlab.com/api/v4/projects/12882469/repository/tags/${CI_COMMIT_TAG}/release)
+curl_release_result=$(curl -v --request POST --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" --header "Content-Type: application/json" --data '{"description": "'$markdown'"}' https://gitlab.com/api/v4/projects/12882469/repository/tags/${CI_COMMIT_TAG}/release)
 
 echo $curl_release_result
 echo ""
