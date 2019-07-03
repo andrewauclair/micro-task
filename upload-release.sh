9eae4df1da5672ec85c0bcf71188aaa4a1dc82a9
Andrew Auclair <mightymalakai33@gmail.com>
2019-07-02 21:51:02 -0400

    Release should now work.

---
+++ upload-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo $CI_COMMIT_TAG
+echo ""
+
+curl_upload_result=$(curl --header "PRIVATE-TOKEN: vRxU9YWpPK3sBSgG4Dmd" --form "file=@target/todo-app-${CI_COMMIT_TAG}.one-jar.jar" https://gitlab.com/api/v4/projects/12882469/uploads)
+
+echo $curl_upload_result
+echo ""
+echo ""
+
+#url=$("${curl_upload_result}" | jq --raw-output '.markdown')
+markdown=$(jq '.markdown' <(echo $curl_upload_result))
+
+echo ""
+echo $markdown
+echo ""
+
+curl_release_result=$(curl --request POST --header "PRIVATE-TOKEN: vRxU9YWpPK3sBSgG4Dmd" --header "Content-Type: application/json" --data '{"description": "$markdown"}' https://gitlab.com/api/v4/projects/12882469/repository/tags/${CI_COMMIT_TAG}/release)
+
+echo $curl_release_result
+echo ""
