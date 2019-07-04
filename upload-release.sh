ad2f82f7f012bca857f6fe2b55baae63589e83ea
Andrew Auclair <mightymalakai33@gmail.com>
2019-07-04 04:48:12 +0000

    Updater can now be used to update to a version found on the gitlab Releases page. This is pretty cool and I think makes more sense than using maven central. Plus now we can use the fat jars made by one-jar and include jna and any other dependencies we want.
    For now the 2 batch files (todo.bat and update.bat) and the todo-app-updater.jar files will just have to be done by hand. Maybe eventually I'll have a way to get these as an install (packed with jpackage and added to the release notes).

---
+++ upload-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo $CI_COMMIT_TAG
+echo ""
+
+curl_upload_result=$(curl -v --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" --form "file=@target/todo-app-${CI_COMMIT_TAG}.one-jar.jar" https://gitlab.com/api/v4/projects/12882469/uploads)
+
+echo $curl_upload_result
+echo ""
+echo ""
+
+#url=$("${curl_upload_result}" | jq --raw-output '.markdown')
+markdown=$(jq '.markdown' <(echo $curl_upload_result))
+
+echo ""
+echo $markdown
+echo ""
+
+curl_release_result=$(curl -v --request POST --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" --header "Content-Type: application/json" --data '{"description": '$markdown'}' https://gitlab.com/api/v4/projects/12882469/repository/tags/${CI_COMMIT_TAG}/release)
+
+echo $curl_release_result
+echo ""
