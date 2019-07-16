4c83d891bdb7760853823ed53bb14fd6cd18e130
andrewauclair <mightymalakai33@gmail.com>
2019-07-15 21:11:07 -0400

    Always showing minutes now if hours is displayed.

--- src/test/java/com/andrewauclair/todo/Commands_Times_Summary_Test.java
+++ src/test/java/com/andrewauclair/todo/Commands_Times_Summary_Test.java
@@ -7,22 +7,22 @@ import java.util.Collections;
 class Commands_Times_Summary_Test extends CommandsBaseTestCase {
 	@Test
 	void times_summary() {
-		tasks.addTask(new Task(1, "Test 1", Task.TaskState.Inactive, Collections.singletonList(new TaskTimes(1234, 1694))));
+		tasks.addTask(new Task(1, "Test 1", Task.TaskState.Inactive, Collections.singletonList(new TaskTimes(0, 3590))));
 		tasks.addTask(new Task(2, "Test 2"));
-		tasks.addTask(new Task(30, "Test 3", Task.TaskState.Inactive, Collections.singletonList(new TaskTimes(90))));
-
-		setTime(4000);
+		tasks.addTask(new Task(30, "Test 3", Task.TaskState.Inactive, Collections.singletonList(new TaskTimes(0))));
+		
+		setTime(3610);
 
 		commands.execute("times --list default --summary");
 
 		assertOutput(
 				"Times summary for list 'default'",
 				"",
-				"01h 05m 10s   30 - 'Test 3'",
-				"    07m 40s   1 - 'Test 1'",
+				"01h 00m 10s   30 - 'Test 3'",
+				"    59m 50s   1 - 'Test 1'",
 				"        00s   2 - 'Test 2'",
 				"",
-				"01h 12m 50s     - Total Time",
+				"02h 00m 00s     - Total Time",
 				""
 		);
 	}
