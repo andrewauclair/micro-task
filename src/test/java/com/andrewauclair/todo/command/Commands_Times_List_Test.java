9674487923afd2175579ce2dbc09bd26192241f7
andrewauclair <mightymalakai33@gmail.com>
2019-12-07 22:18:00 -0500

    Some cleanup before we move on to other stuff.

--- src/test/java/com/andrewauclair/todo/command/Commands_Times_List_Test.java
+++ src/test/java/com/andrewauclair/todo/command/Commands_Times_List_Test.java
@@ -3,23 +3,24 @@ package com.andrewauclair.todo.command;
 
 import org.junit.jupiter.api.Test;
 
+// TODO I would like a more detailed broken down set of tests for the times command variations
 class Commands_Times_List_Test extends CommandsBaseTestCase {
 	@Test
 	void times_on_default_list() {
 		addTaskWithTimes("Task 1", 1000, 2000);
 		addTaskTimes(1, 4000, 4500);
-
+		
 		addTaskWithTimes("Task 2", 2000, 2800);
 		addTaskTimes(2, 3500, 4600);
-
+		
 		tasks.addList("test");
 		tasks.setActiveList("test");
-
+		
 		addTaskWithTimes("Task 3", 1000, 3000);
 		addTaskTimes(3, 3800, 4700);
 		
 		commands.execute(printStream, "times --list default");
-
+		
 		assertOutput(
 				"Times for list 'default'",
 				"",
