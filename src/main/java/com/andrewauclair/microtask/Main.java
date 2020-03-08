4c6ab2cd7d9452262d7497be28d7fbee02758d3d
Andrew Auclair <mightymalakai33@gmail.com>
2020-03-08 03:02:05 -0400

    Removing the generateTestData I used when I was testing load times. The load times are very fast and I hardly ever restart the application. So there's no need to have this laying around.

--- src/main/java/com/andrewauclair/microtask/Main.java
+++ src/main/java/com/andrewauclair/microtask/Main.java
@@ -26,7 +26,6 @@ import java.net.Socket;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -49,9 +48,9 @@ public final class Main {
 		PicocliCommands picocliCommands = new PicocliCommands(Paths.get(""), cmd);
 		systemCompleter.add(picocliCommands.compileCompleters());
 		systemCompleter.compile();
-		
+
 		boolean loadHistory = false;
-		
+
 		if (lineReader != null) {
 			try {
 				lineReader.getHistory().save();
@@ -62,7 +61,7 @@ public final class Main {
 			}
 		}
 		lineReader = buildLineReader(systemCompleter, terminal);
-		
+
 		if (loadHistory) {
 			try {
 				lineReader.getHistory().load();
@@ -71,22 +70,22 @@ public final class Main {
 				e.printStackTrace();
 			}
 		}
-		
+
 		builtins.setLineReader(lineReader);
 		bindCtrlBackspace(lineReader);
-		
+
 		DescriptionGenerator descriptionGenerator = new DescriptionGenerator(builtins, picocliCommands);
 		new Widgets.TailTipWidgets(lineReader, descriptionGenerator::commandDescription, 5, Widgets.TailTipWidgets.TipType.COMPLETER);
-	
+
 	}
-	
+
 	private static final char BACKSPACE_KEY = '\u0008';
 	private static final AtomicBoolean runningCommand = new AtomicBoolean(false);
-	
+
 	private static final OSInterfaceImpl osInterface = new OSInterfaceImpl();
-	
+
 	private final Tasks tasks;
-	
+
 	private Main() throws Exception {
 		osInterface.createTerminal();
 
@@ -97,7 +96,7 @@ public final class Main {
 
 		ServerSocket server = new ServerSocket(5678);
 		Socket accept = server.accept();
-		
+
 		DataOutputStream serverOut = new DataOutputStream(accept.getOutputStream());
 
 		boolean loadSuccessful = tasks.load(new TaskLoader(tasks, new TaskReader(osInterface), osInterface), commands);
@@ -112,11 +111,11 @@ public final class Main {
 		if (loadSuccessful) {
 			lineReader.getBuiltinWidgets().get(LineReader.CLEAR_SCREEN).apply();
 		}
-		
+
 		if (tasks.getActiveTaskID() != Tasks.NO_ACTIVE_TASK) {
 			// set active list to the list of the active task
 			tasks.setActiveList(tasks.getActiveTaskList());
-			
+
 			// set active group to the group of the active task
 			tasks.switchGroup(tasks.getGroupForList(tasks.getActiveTaskList()).getFullPath());
 		}
@@ -126,7 +125,7 @@ public final class Main {
 		while (true) {
 			try {
 				String command = lineReader.readLine(commands.getPrompt());
-				
+
 				if (command.equals("proj-feat-assign")) {
 					manualProjectFeatureAssign(tasks);
 				}
@@ -137,9 +136,6 @@ public final class Main {
 
 				if (command.equals("export")) {
 					exportData(tasks);
-				}
-				else if (command.equals("test-data")) {
-					generateTestData(tasks);
 				}
 				else if (command.startsWith("status")) {
 					serverOut.write(StatusConsole.TransferType.Command.ordinal());
@@ -184,7 +180,7 @@ public final class Main {
 			new Main();
 		}
 	}
-	
+
 	private static LineReader buildLineReader(Completers.SystemCompleter systemCompleter, Terminal terminal) {
 		return LineReaderBuilder.builder()
 				.terminal(terminal)
@@ -195,15 +191,15 @@ public final class Main {
 				.variable(LineReader.HISTORY_FILE, "history.txt")
 				.build();
 	}
-	
+
 	private void bindCtrlBackspace(LineReader lineReader) {
 		KeyMap<Binding> main = lineReader.getKeyMaps().get(LineReader.MAIN);
-		
+
 		Widget widget = lineReader.getBuiltinWidgets().get(LineReader.BACKWARD_KILL_WORD);
-		
+
 		main.bind((Widget) () -> {
 			short keyState = User32.INSTANCE.GetAsyncKeyState(WinUser.VK_LCONTROL);
-			
+
 			if ((keyState & 0x8000) == 0) {
 				return lineReader.getBuffer().backspace();
 			}
@@ -212,57 +208,36 @@ public final class Main {
 			}
 		}, KeyMap.ctrl(BACKSPACE_KEY));
 	}
-	
+
 	// temporary function that we will use to assign a project and feature to all existing task times at work
 	// I'll remove this once I can verify that everything is working well for projects and features
 	private void manualProjectFeatureAssign(Tasks tasks) {
 		TaskWriter writer = new TaskWriter(Main.osInterface);
-		
+
 		for (Task task : tasks.getAllTasks()) {
 			List<TaskTimes> oldTimes = task.getStartStopTimes();
 			List<TaskTimes> newTimes = new ArrayList<>();
-			
+
 			newTimes.add(task.getAllTimes().get(0));
-			
+
 			for (TaskTimes time : oldTimes) {
 				newTimes.add(new TaskTimes(time.start, time.stop, tasks.getProjectForTask(task.id), tasks.getFeatureForTask(task.id)));
 			}
-			
+
 			Task newTask = new Task(task.id, task.task, task.state, newTimes, task.isRecurring());
-			
+
 			writer.writeTask(newTask, "git-data/tasks/" + tasks.findListForTask(task.id).getFullPath() + "/" + task.id + ".txt");
 		}
 		System.exit(0);
 	}
-	
-	private void generateTestData(Tasks tasks) {
-		// create 100 groups with 10000 tasks spread randomly between them
-		
-		// just testing load times for now
-		
-		// next we'll randomly generate times for the 10000 tasks
-		
-		for (int i = 0; i < 100; i++) {
-			tasks.createGroup("group-" + (i + 1));
-			tasks.addList("group-" + (i + 1) + "/list-" + (i + 1), true);
-		}
-		
-		Random random = new Random();
-		
-		for (int i = 0; i < 10000; i++) {
-			int group = random.nextInt(100);
-			
-			tasks.addTask("Test " + (i + 1), "group-" + (group + 1) + "/list-" + (group + 1));
-		}
-	}
-	
+
 	// Export data with generic names, this will remove any possible proprietary data
 	// Tasks will be exported as X - 'Task X'
 	// Groups will be exported as 'group-x'
 	// Lists will be exported as 'list-x'
 	private void exportData(Tasks tasks) {
 		exportGroup(tasks, tasks.getRootGroup(), "/", 1, 1, Main.osInterface);
-		
+
 		try (OutputStream outputStream = ((OSInterface) Main.osInterface).createOutputStream("git-data-export/next-id.txt")) {
 			outputStream.write(String.valueOf(tasks.nextID()).getBytes());
 		}
@@ -270,10 +245,10 @@ public final class Main {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void exportGroup(Tasks tasks, TaskGroup group, String path, int groupNum, int listNum, OSInterface osInterface) {
 		tasks.writeGroupInfoFile(group, "git-data-export");
-		
+
 		for (TaskContainer child : group.getChildren()) {
 			if (child instanceof TaskGroup) {
 				String name = "group-" + groupNum;
@@ -283,20 +258,20 @@ public final class Main {
 			else if (child instanceof TaskList) {
 				String name = "list-" + listNum;
 				listNum++;
-				
+
 				exportList(tasks, (TaskList) child, path + name, osInterface);
 			}
 		}
 	}
-	
+
 	private void exportList(Tasks tasks, TaskList list, String path, OSInterface osInterface) {
 		tasks.writeListInfoFile(list, "git-data-export");
-		
+
 		TaskWriter writer = new TaskWriter(osInterface);
 
 		for (Task task : list.getTasks()) {
 			Task strippedTask = new TaskBuilder(task).rename("Task " + task.id);
-			
+
 			writer.writeTask(strippedTask, "git-data-export/tasks" + path + "/" + task.id + ".txt");
 		}
 	}
@@ -304,18 +279,18 @@ public final class Main {
 	// TODO Find a way to test this
 	private boolean requiresTaskUpdate() {
 		String currentVersion = "";
-		
+
 		try {
 			currentVersion = Main.osInterface.getVersion();
 		}
 		catch (IOException ignored) {
 		}
-		
+
 		try (InputStream inputStream = Main.osInterface.createInputStream("git-data/task-data-version.txt")) {
 			Scanner scanner = new Scanner(inputStream);
-			
+
 			String dataVersion = scanner.nextLine();
-			
+
 			return !currentVersion.equals(dataVersion);
 		}
 		catch (Exception ignored) {
@@ -323,7 +298,7 @@ public final class Main {
 		// if the file doesn't exist, then yes, we need to update
 		return true;
 	}
-	
+
 	/**
 	 * Top-level command that just prints help.
 	 */
@@ -335,15 +310,15 @@ public final class Main {
 			subcommands = {HelpCommand.class})
 	public static final class CliCommands implements Runnable {
 		PrintWriter out;
-		
+
 		public CliCommands() {
 		}
-		
+
 		public void run() {
 			out.println(new CommandLine(this).getUsageMessage());
 		}
 	}
-	
+
 	/**
 	 * Provide command descriptions for JLine TailTipWidgets
 	 * to be displayed in the status bar.
@@ -351,23 +326,23 @@ public final class Main {
 	private static final class DescriptionGenerator {
 		final Builtins builtins;
 		final PicocliCommands picocli;
-		
+
 		DescriptionGenerator(Builtins builtins, PicocliCommands picocli) {
 			this.builtins = builtins;
 			this.picocli = picocli;
 		}
-		
+
 		Widgets.CmdDesc commandDescription(Widgets.CmdLine line) {
 			Widgets.CmdDesc out = null;
-			
+
 			if (line.getDescriptionType() == Widgets.CmdLine.DescriptionType.COMMAND) {
 				String cmd = Parser.getCommand(line.getArgs().get(0));
-				
+
 				if (builtins.hasCommand(cmd) || picocli.hasCommand(cmd)) {
 					out = builtins.commandDescription(cmd);
 				}
 			}
-			
+
 			return out;
 		}
 	}
