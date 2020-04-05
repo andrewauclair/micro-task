7badec24de736352c97b11cc552bb75a35d71173
andrewauclair <mightymalakai33@gmail.com>
2020-04-05 15:19:19 -0400

    [skip-ci] Moved file to where it's supposed to be.

--- src/test/java/com/andrewauclair/microtask/command/Commands_Times_Log_Test.java
+++ src/test/java/com/andrewauclair/microtask/command/Commands_Times_Log_Test.java
@@ -1,5 +1,5 @@
 // Copyright (C) 2020 Andrew Auclair - All Rights Reserved
-package com.andrewauclair.todo.command;
+package com.andrewauclair.microtask.command;
 
 import com.andrewauclair.microtask.command.Commands_Times_BaseTestCase;
 import com.andrewauclair.microtask.task.Task;
@@ -12,7 +12,6 @@ import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
@@ -21,12 +20,7 @@ class Commands_Times_Log_Test extends Commands_Times_BaseTestCase {
 	@Test
 	void times_log_for_a_day__today() {
 		setTime(june17_8_am);
-		
-		List<TaskTimes> addTime = Collections.singletonList(new TaskTimes(0));
-		
-//		tasks.addTask("Test 1");
-//		tasks.startTask(1, false);
-		
+
 		String list = "/default";
 		
 		when(mockTaskTimesFilter.getData()).thenReturn(
@@ -107,38 +101,38 @@ class Commands_Times_Log_Test extends Commands_Times_BaseTestCase {
 		assertOutput(
 				"Times log for 06/17/2019",
 				"",
-				"08:02:35 AM   Added 1 - 'Test 1'",
-				"08:02:40 AM   Added 2 - 'Test 2'",
-				"08:02:50 AM   Added 3 - 'Test 3'",
-				"08:03:05 AM   Started 1 - 'Test 1'",
-				"08:38:00 AM   Stopped 1 - 'Test 1'",
-				"08:38:00 AM   Started 2 - 'Test 2'",
-				"09:15:14 AM   Finished 2 - 'Test 2'",
-				"09:15:14 AM   Started 1 - 'Test 1'",
-				"10:27:11 AM   Finished 1 - 'Test 1'",
-				"10:27:11 AM   Started 3 - 'Test 3'",
-				"10:41:19 AM   Added 4 - 'Test 4'",
-				"10:42:22 AM   Added 5 - 'Test 5'",
-				"11:04:40 AM   Added 6 - 'Test 6'",
-				"11:05:00 AM   Stopped 3 - 'Test 3'",
-				"11:05:00 AM   Started 6 - 'Test 6'",
-				"12:01:12 PM   Stopped 6 - 'Test 6'",
-				"12:35:55 PM   Started 8 - 'Test 8'",
-				"12:40:35 PM   Added 7 - 'Test 7'",
-				"12:43:35 PM   Added 8 - 'Test 8'",
-				"01:01:01 PM   Finished 8 - 'Test 8'",
-				"01:01:01 PM   Started 4 - 'Test 4'",
-				"01:11:00 PM   Finished 4 - 'Test 4'",
-				"01:11:00 PM   Started 7 - 'Test 7'",
-				"02:15:48 PM   Stopped 7 - 'Test 7'",
-				"02:15:48 PM   Started 3 - 'Test 3'",
-				"02:30:35 PM   Added 9 - 'Test 9'",
-				"03:11:49 PM   Added 10 - 'Test 10'",
-				"03:15:00 PM   Finished 3 - 'Test 3'",
-				"",
-				"10 added",
-				" 8 started",
-				" 5 finished",
+				"07:02:35 AM   Added 1 - 'Test 1'",
+				"07:02:40 AM   Added 2 - 'Test 2'",
+				"07:02:50 AM   Added 3 - 'Test 3'",
+				"07:03:05 AM   Started 1 - 'Test 1'",
+				"07:38:00 AM   Stopped 1 - 'Test 1'",
+				"07:38:00 AM   Started 2 - 'Test 2'",
+				"08:15:14 AM   Finished 2 - 'Test 2'",
+				"08:15:14 AM   Started 1 - 'Test 1'",
+				"09:27:11 AM   Finished 1 - 'Test 1'",
+				"09:27:11 AM   Started 3 - 'Test 3'",
+				"09:41:19 AM   Added 4 - 'Test 4'",
+				"09:42:22 AM   Added 5 - 'Test 5'",
+				"10:04:40 AM   Added 6 - 'Test 6'",
+				"10:05:00 AM   Stopped 3 - 'Test 3'",
+				"10:05:00 AM   Started 6 - 'Test 6'",
+				"11:01:12 PM   Stopped 6 - 'Test 6'",
+				"11:35:55 PM   Started 8 - 'Test 8'",
+				"11:40:35 PM   Added 7 - 'Test 7'",
+				"11:43:35 PM   Added 8 - 'Test 8'",
+				"12:01:01 PM   Finished 8 - 'Test 8'",
+				"12:01:01 PM   Started 4 - 'Test 4'",
+				"12:11:00 PM   Finished 4 - 'Test 4'",
+				"12:11:00 PM   Started 7 - 'Test 7'",
+				"01:15:48 PM   Stopped 7 - 'Test 7'",
+				"01:15:48 PM   Started 3 - 'Test 3'",
+				"01:30:35 PM   Added 9 - 'Test 9'",
+				"02:11:49 PM   Added 10 - 'Test 10'",
+				"02:15:00 PM   Finished 3 - 'Test 3'",
+//				"",
+//				"10 added",
+//				" 8 started",
+//				" 5 finished",
 				""
 		);
 	}
