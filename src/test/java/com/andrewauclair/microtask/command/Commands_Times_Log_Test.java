35c3a1c66928a71cc61dfe29117727af7d2e24b6
andrewauclair <mightymalakai33@gmail.com>
2020-04-05 21:15:51 -0400

    [skip-ci] Fixing some test times.

--- src/test/java/com/andrewauclair/microtask/command/Commands_Times_Log_Test.java
+++ src/test/java/com/andrewauclair/microtask/command/Commands_Times_Log_Test.java
@@ -27,66 +27,66 @@ class Commands_Times_Log_Test extends Commands_Times_BaseTestCase {
 				Arrays.asList(
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(1, "Test 1", TaskState.Finished,
 								Arrays.asList(
-										new TaskTimes(1560772955), // add 08:02:35 AM
-										new TaskTimes(1560772925, 1560775080), // start - stop 08:03:05 AM - 08:38:00 AM
-										new TaskTimes(1560777314, 1560781631), // start - stop 09:15:14 AM - 10:27:11 AM
-										new TaskTimes(1560781631) // finish 10:27:11 AM
+										new TaskTimes(1560772955), // add 07:02:35 AM
+										new TaskTimes(1560772985, 1560775080), // start - stop 07:03:05 AM - 07:38:00 AM
+										new TaskTimes(1560777314, 1560781631), // start - stop 08:15:14 AM - 09:27:11 AM
+										new TaskTimes(1560781631) // finish 09:27:11 AM
 								)
 						), list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(2, "Test 2", TaskState.Finished,
 								Arrays.asList(
-										new TaskTimes(1560772960), // add 08:02:40 AM
-										new TaskTimes(1560775080, 1560777314), // start - stop 08:38:00 AM - 09:15:14 AM
-										new TaskTimes(1560777314) // finish 09:15:14 AM
+										new TaskTimes(1560772960), // add 07:02:40 AM
+										new TaskTimes(1560775080, 1560777314), // start - stop 07:38:00 AM - 08:15:14 AM
+										new TaskTimes(1560777314) // finish 08:15:14 AM
 								)
 						), list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(3, "Test 3", TaskState.Finished,
 								Arrays.asList(
-										new TaskTimes(1560772970), // add 08:02:50 AM
-										new TaskTimes(1560781631, 1560783900), // start - stop 10:27:11 AM - 11:05:00 AM
-										new TaskTimes(1560795348, 1560798900), // start - stop 02:15:48 PM - 03:15:00 PM
-										new TaskTimes(1560798900) // finish 03:15:00 PM
+										new TaskTimes(1560772970), // add 07:02:50 AM
+										new TaskTimes(1560781631, 1560783900), // start - stop 09:27:11 AM - 10:05:00 AM
+										new TaskTimes(1560795348, 1560798900), // start - stop 01:15:48 PM - 02:15:00 PM
+										new TaskTimes(1560798900) // finish 02:15:00 PM
 								)
 						), list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(4, "Test 4", TaskState.Finished,
 								Arrays.asList(
-										new TaskTimes(1560782479), // add 10:41:19 AM
-										new TaskTimes(1560790861, 1560791460), // start - stop 01:01:01 PM - 01:11:00 PM
-										new TaskTimes(1560791460) // finish 01:11:00 PM
+										new TaskTimes(1560782479), // add 09:41:19 AM
+										new TaskTimes(1560790861, 1560791460), // start - stop 12:01:01 PM - 12:11:00 PM
+										new TaskTimes(1560791460) // finish 12:11:00 PM
 								)
 						),list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(5, "Test 5", TaskState.Inactive,
 								Collections.singletonList(
-										new TaskTimes(1560782542) // add 10:42:22 AM
+										new TaskTimes(1560782542) // add 09:42:22 AM
 								)
 						),list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(6, "Test 6", TaskState.Inactive,
 								Arrays.asList(
-										new TaskTimes(1560783880), // add 11:04:40 AM
-										new TaskTimes(1560783900, 1560787272) // start - stop 11:05:00 AM - 12:01:12 PM
+										new TaskTimes(1560783880), // add 10:04:40 AM
+										new TaskTimes(1560783900, 1560787272) // start - stop 10:05:00 AM - 11:01:12 PM
 								)
 						),list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(7, "Test 7", TaskState.Inactive,
 								Arrays.asList(
-										new TaskTimes(1560789635), // add 12:40:35 PM
-										new TaskTimes(1560791460, 1560795348) // start - stop 01:11:00 PM - 02:15:48 PM
+										new TaskTimes(1560789635), // add 11:40:35 PM
+										new TaskTimes(1560791460, 1560795348) // start - stop 12:11:00 PM - 01:15:48 PM
 								)
 						),list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(8, "Test 8", TaskState.Finished,
 								Arrays.asList(
-										new TaskTimes(1560789815), // add 12:43:35 PM
-										new TaskTimes(1560789355, 1560790861), // start - stop 12:35:55 PM - 01:01:01 PM
-										new TaskTimes(1560790861) // finish 01:01:01 PM
+										new TaskTimes(1560789815), // add 11:43:35 PM
+										new TaskTimes(1560789355, 1560790861), // start - stop 11:35:55 PM - 12:01:01 PM
+										new TaskTimes(1560790861) // finish 12:01:01 PM
 								)
 						),list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(9, "Test 9", TaskState.Inactive,
 								Collections.singletonList(
-										new TaskTimes(1560796235) // add 02:30:35 PM
+										new TaskTimes(1560796235) // add 01:30:35 PM
 								)
 						),list),
 						new TaskTimesFilter.TaskTimeFilterResult(0, new Task(10, "Test 10", TaskState.Inactive,
 								Collections.singletonList(
-										new TaskTimes(1560798709) // add 03:11:49 PM
+										new TaskTimes(1560798709) // add 02:11:49 PM
 								)
 						),list)
 				)
