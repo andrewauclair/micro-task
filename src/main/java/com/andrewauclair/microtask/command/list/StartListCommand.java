9d6f3fe4aec39491e8f678180114e77360e89d54
Andrew Auclair <mightymalakai33@gmail.com>
2020-11-04 17:03:27 -0500

    "start list" should now have auto completion.

--- src/main/java/com/andrewauclair/microtask/command/list/StartListCommand.java
+++ src/main/java/com/andrewauclair/microtask/command/list/StartListCommand.java
@@ -1,6 +1,7 @@
 // Copyright (C) 2020 Andrew Auclair - All Rights Reserved
 package com.andrewauclair.microtask.command.list;
 
+import com.andrewauclair.microtask.jline.ListCompleter;
 import com.andrewauclair.microtask.task.Tasks;
 import com.andrewauclair.microtask.task.list.name.ExistingListName;
 import picocli.CommandLine;
@@ -12,7 +13,7 @@ public class StartListCommand implements Runnable {
 	@CommandLine.Option(names = {"-h", "--help"}, description = "Show this help message.", usageHelp = true)
 	private boolean help;
 
-	@CommandLine.Parameters(index = "0", description = "The list to start.")
+	@CommandLine.Parameters(index = "0", completionCandidates = ListCompleter.class, description = "The list to start.")
 	private ExistingListName list;
 
 	public StartListCommand(Tasks tasks) {
