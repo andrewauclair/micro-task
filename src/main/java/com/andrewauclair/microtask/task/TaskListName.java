6b981df7747eb1b44eeb27b48c0c70d25891b2ae
Andrew Auclair <mightymalakai33@gmail.com>
2022-02-10 19:27:40 -0500

    Group and list names are now checked for characters that are illegal in windows file names.

--- src/main/java/com/andrewauclair/microtask/task/TaskListName.java
+++ src/main/java/com/andrewauclair/microtask/task/TaskListName.java
@@ -3,9 +3,14 @@ package com.andrewauclair.microtask.task;
 
 import com.andrewauclair.microtask.task.group.name.ExistingGroupName;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static com.andrewauclair.microtask.task.TaskGroup.ROOT_PATH;
 
 public abstract class TaskListName {
+	private static final List<Character> illegal_characters = Arrays.asList('\\', '<', '>', ':', '\"', '\\', '|', '?', '*');
+
 	private final String absoluteName;
 	private final String shortName;
 
@@ -14,6 +19,12 @@ public abstract class TaskListName {
 	public TaskListName(Tasks tasks, String name) {
 		if (name.endsWith("/")) {
 			throw new RuntimeException("List name must not end in /");
+		}
+
+		for (final char character : illegal_characters) {
+			if (name.contains(String.valueOf(character))) {
+				throw new RuntimeException("Illegal character in list name: '" + character + "'");
+			}
 		}
 
 		this.absoluteName = absoluteName(tasks, name).toLowerCase();
--- src/test/java/com/andrewauclair/microtask/task/TaskGroupName_Test.java
+++ src/test/java/com/andrewauclair/microtask/task/TaskGroupName_Test.java
@@ -13,4 +13,60 @@ class TaskGroupName_Test extends TaskBaseTestCase {
 
 		assertEquals("Group name must end in /", runtimeException.getMessage());
 	}
+
+	@Test
+	void backslashes_are_not_allowed() {
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> new TaskGroupName(tasks, "te\\st/"){});
+
+		assertEquals("Illegal character in group name: '\\'", runtimeException.getMessage());
+	}
+
+	@Test
+	void less_thans_are_not_allowed() {
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> new TaskGroupName(tasks, "te<st/"){});
+
+		assertEquals("Illegal character in group name: '<'", runtimeException.getMessage());
+	}
+
+	@Test
+	void greater_thans_are_not_allowed() {
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> new TaskGroupName(tasks, "te>st/"){});
+
+		assertEquals("Illegal character in group name: '>'", runtimeException.getMessage());
+	}
+
+	@Test
+	void colons_are_not_allowed() {
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> new TaskGroupName(tasks, "te:st/"){});
+
+		assertEquals("Illegal character in group name: ':'", runtimeException.getMessage());
+	}
+
+	@Test
+	void double_quotes_are_not_allowed() {
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> new TaskGroupName(tasks, "te\"st/"){});
+
+		assertEquals("Illegal character in group name: '\"'", runtimeException.getMessage());
+	}
+
+	@Test
+	void vertical_bar_pipes_are_not_allowed() {
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> new TaskGroupName(tasks, "te|st/"){});
+
+		assertEquals("Illegal character in group name: '|'", runtimeException.getMessage());
+	}
+
+	@Test
+	void question_marks_are_not_allowed() {
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> new TaskGroupName(tasks, "te?st/"){});
+
+		assertEquals("Illegal character in group name: '?'", runtimeException.getMessage());
+	}
+
+	@Test
+	void asterisks_are_not_allowed() {
+		RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> new TaskGroupName(tasks, "te*st/"){});
+
+		assertEquals("Illegal character in group name: '*'", runtimeException.getMessage());
+	}
 }
